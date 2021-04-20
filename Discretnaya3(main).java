0a27faba4f6c9dff14cb6c676bf3f10f5776729c
UnderstandableHaveANiceWay <70845152+UnderstandableHaveANiceWay@users.noreply.github.com>
2021-04-20 12:58:56 +0300

    Update Discretnaya3(main).java

--- Discretnaya3(main).java
+++ Discretnaya3(main).java
@@ -4,6 +4,350 @@ import java.util.ArrayList;
 
 
 public class Lab2and3 {
+    import java.util.ArrayList;
+import java.util.Stack;
+    
+
+public class Edge {
+
+    public int weight;
+    public Vertex from,to;
+    public Edge(Vertex From,Vertex To) {
+
+        from=From;
+        to=To;
+        weight=1;
+    }
+}
+public class Vertex {
+
+    public int number;
+
+    public Vertex(int Number) {
+        number = Number;
+    }
+}
+
+public class Graph {
+
+    ArrayList<Vertex> Vertexes=new ArrayList<>();
+
+    ArrayList<Edge> Edges=new ArrayList<>();
+
+    public void addVertex(Vertex vertex){
+
+        Vertexes.add(vertex);
+    }
+
+    public void addEdge(Vertex From,Vertex To){
+
+        Edge edge=new Edge(From,To);
+
+        Edges.add(edge);
+    }
+
+    public int[][] getMatrix() {
+
+        int s= Vertexes.size();
+        int[][] matrix=new int[s][s];
+
+        for(Edge E:Edges){
+
+            int row=E.from.number-1;
+            int column=E.to.number-1;
+
+            matrix[row][column]=E.weight;
+        }
+        return matrix;
+    }
+    public Vertex vertexSearch(int number){
+
+        for(Vertex v:Vertexes){
+
+            if(v.number==number)return v;
+        }
+
+        return null;
+    }
+    public ArrayList<Vertex> getVertexList(Vertex vertex){
+        ArrayList<Vertex> result=new ArrayList<>();
+        for(Edge E:Edges){
+            if(E.from==vertex){
+                result.add(E.to);
+            }
+        }
+        return result;
+    }
+    public void smeMatrix(){
+        int m[][]=getMatrix(),I=Vertexes.size();
+
+        for(int i=0;i<I;++i){
+            for(int j=0;j<I;++j){
+
+                System.out.print(m[i][j]);
+            }
+            System.out.println();
+        }
+    }
+    public void smeList(){
+        int i=1;
+        for(Vertex V:Vertexes){
+            ArrayList<Vertex> out=getVertexList(V);
+            System.out.print(i+" - ");
+            for(Vertex v:out){
+                System.out.print(v.number+",");
+            }
+            System.out.println("0;");
+            ++i;
+        }
+    }
+    public boolean containEdge(Vertex from, Vertex to){
+        for(Edge e:Edges){
+            if(e.from==from&&e.to==to){
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean dfs(Vertex start,Vertex finish){
+        if(start==null||finish==null)return false;
+        else if(start==finish)return true;
+        Stack<Vertex> stack=new Stack<>();
+
+        stack.push(start);
+
+        while(!stack.empty()){
+            Vertex v=stack.pop();
+
+            ArrayList<Vertex> getV=getVertexList(v);
+
+            if(!getV.isEmpty()){
+                for(Vertex V:getV){
+                    if(getV.get(0)==finish) return true;
+                    if(!stack.contains(V)&&V!=finish)stack.push(V);
+                }
+            }
+        }
+        return false;
+    }
+    public boolean bfs(Vertex start,Vertex finish){
+        if(start==null||finish==null)return false;
+        else if(start==finish)return true;
+        ArrayList<Vertex> list=new ArrayList<>();
+
+        list.add(start);
+
+        for(int i=0;i<list.size();++i){
+            Vertex v=list.get(i);
+
+            ArrayList<Vertex> getV=getVertexList(v);
+
+            if(!getV.isEmpty()){
+                for (Vertex V : getV) {
+                    if (!list.contains(V))list.add(V);
+                    if(V==finish)return true;
+                }
+            }
+        }
+        return false;
+    }
+    public ArrayList<ArrayList<Vertex>> fordMin(Vertex start, Vertex finish){
+        if(!bfs(start,finish)||start==finish)return null;
+
+        int vWeight[]=new int[Vertexes.size()],sum=0,min=999999999;
+        boolean dupl=false;
+        ArrayList<Vertex>list=new ArrayList<>();
+        Vertex lastAdded=null;
+
+        list.add(start);
+        vWeight[0]=0;
+        for(int i=1;i<vWeight.length;++i){
+            vWeight[i]=999999999;
+        }
+
+
+        for (int i=0;i<list.size();++i){
+            Vertex v=list.get(i);
+
+            ArrayList<Vertex> getV=getVertexList(v);
+
+            list.addAll(getV);
+
+            for(Edge E:Edges){
+                if(E.from==v) {
+                    if (vWeight[E.from.number - 1] + vWeight[E.to.number - 1] > E.weight&&vWeight[E.from.number-1]+E.weight<vWeight[E.to.number-1])
+                        vWeight[E.to.number - 1] = vWeight[E.from.number - 1] + E.weight;
+                }
+            }
+        }
+
+        Stack<Vertex> stack=new Stack<>();
+        ArrayList<ArrayList<Vertex>>ways=new ArrayList<>();
+        ArrayList<Vertex> getV;
+
+        list.clear();
+        list.add(start);
+
+        stack.add(start);
+
+        while (!stack.empty()&&!list.isEmpty()){
+            Vertex v = stack.pop();
+
+            getV = getVertexList(v);
+
+            for (Vertex w:getV) {
+                if (bfs(w, finish)) stack.push(w);
+            }
+
+            for (Edge E : Edges) {
+                if (E.to == v && E.from==list.get(list.size()-1) && vWeight[E.to.number - 1] - vWeight[E.from.number - 1] == E.weight) {
+                    list.add(v);
+                    sum+=E.weight;
+                    break;
+                }
+            }
+
+            if(v!=finish)continue;
+
+            if(!list.isEmpty()&&list.contains(finish)) {
+                if(sum<min)min=sum;
+                for(ArrayList<Vertex> s:ways){
+                    if(s.equals(list)) {
+                        dupl = true;
+                        break;
+                    }
+                }
+                if(!dupl)ways.add((ArrayList<Vertex>)list.clone());
+                else if(stack.size()>1)stack.push(lastAdded);
+            }
+
+            Vertex w;
+
+            while(!list.isEmpty()&&!stack.isEmpty()){
+
+                w=list.get(list.size()-1);
+
+                if((!bfs(w,stack.lastElement())||dupl)&&list.size()>1){
+                    for(Edge e:Edges){
+                        if(e.to==w && e.from==list.get(list.indexOf(w)-1)) {
+                            sum -= e.weight;
+                            break;
+                        }
+                    }
+                    list.remove(w);
+
+                    if(bfs(w,stack.lastElement())&&w!=stack.lastElement()){
+                        dupl=false;
+                        break;
+                    }
+                }else{
+                    if(list.size()==1&&dupl)dupl=false;
+                    lastAdded=stack.lastElement();
+                    break;
+                }
+            }
+        }
+        System.out.println("min= "+min);
+        return ways;
+    }
+    public ArrayList<ArrayList<Vertex>> belmanMin(Vertex start, Vertex finish){
+        if(!bfs(start,finish)||start==finish)return null;
+
+        ArrayList<ArrayList<Vertex>> ways=new ArrayList<>();
+        int distance[][]=getMatrix(),vector[][]=new int[Vertexes.size()][Vertexes.size()],vertexCount=Vertexes.size(),min=999999999,workVector=0;
+        boolean dupl=true;
+        Vertex lastAdded=null;
+
+        for(int i=0;i<vertexCount;++i){
+            for(int j=0;j<vertexCount;++j){
+                if(i!=j&&distance[i][j]==0)distance[i][j]=999999999;
+            }
+        }
+
+        for(int j=0;j<vertexCount;++j){
+            vector[0][j]=distance[j][finish.number-1];
+        }
+
+        for(int i=1;i<vertexCount;++i){
+            for(int j=0;j<vertexCount;++j){
+                for(int z=0;z<vertexCount;++z){
+                    int rez=distance[j][z]+vector[i-1][z];
+                    if(min>rez)min=rez;
+                }
+                vector[i][j]=min;
+                min=999999999;
+            }
+            for(int j=0;j<vertexCount;++j){
+                if(vector[i][j]!=vector[i-1][j]) {
+                    dupl = false;
+                    break;
+                }
+            }
+            if(dupl) {
+                workVector=i;
+                break;
+            }
+            dupl=true;
+        }
+
+        ArrayList<Vertex> list=new ArrayList<>();
+        Stack<Vertex> stack=new Stack<>();
+
+        stack.add(start);
+        list.add(start);
+        ArrayList<Vertex>getV;
+        dupl=false;
+
+        while(!stack.isEmpty()){
+            Vertex v = stack.pop();
+
+            getV = getVertexList(v);
+
+            for (Vertex w:getV) {
+                if (bfs(w, finish)) stack.push(w);
+            }
+            if(distance[list.get(list.size()-1).number-1][v.number-1]==vector[workVector][list.get(list.size()-1).number-1]-vector[workVector][v.number-1]&&!list.contains(v))list.add(v);
+
+            if(v!=finish)continue;
+
+            if(!list.isEmpty()&&list.contains(finish)) {
+                for(ArrayList<Vertex> s:ways){
+                    if(s.equals(list)) {
+                        dupl = true;
+                        break;
+                    }
+                }
+                if(!dupl)ways.add((ArrayList<Vertex>)list.clone());
+                else if(stack.size()>1)stack.push(lastAdded);
+            }
+
+            Vertex w;
+
+            while(!list.isEmpty()&&!stack.isEmpty()){
+
+                w=list.get(list.size()-1);
+
+                if((!bfs(w,stack.lastElement())||dupl)&&list.size()>1){
+
+                    list.remove(w);
+
+                    if(bfs(w,stack.lastElement())&&w!=stack.lastElement()){
+                        dupl=false;
+                        break;
+                    }
+                }else{
+                    if(list.size()==1&&dupl)dupl=false;
+                    lastAdded=stack.lastElement();
+                    break;
+                }
+            }
+        }
+
+        System.out.println("min= "+vector[workVector][0]);
+        return ways;
+    }
+}
+
     public static void menu(){
         System.out.println("1.Меню");
         System.out.println("2.Создать граф");
